4d115db6825501dcc1f3cfa5e862f750b3523e03
anatoliisharov <sharov.anatoliy@gmail.com>
2023-07-19 16:36:42 +0300

    successfully without intermadiat and not with, split stream to split the sink

--- kafka-flink-postgresql-gradle/src/main/java/com/example/sharov/anatoliy/DataStreamJob.java
+++ kafka-flink-postgresql-gradle/src/main/java/com/example/sharov/anatoliy/DataStreamJob.java
@@ -23,49 +23,53 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
 import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
 import org.apache.flink.connector.jdbc.JdbcSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.connector.kafka.source.reader.deserializer.KafkaRecordDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 /**
  * Skeleton for a Flink DataStream Job.
  *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ * <p>
+ * For a tutorial how to write a Flink application, check the tutorials and
+ * examples on the <a href="https://flink.apache.org">Flink Website</a>.
  *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
+ * <p>
+ * To package your application into a JAR file for execution, run 'mvn clean
+ * package' on the command line.
  *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ * <p>
+ * If you change the name of the main class (with the public static void
+ * main(String[] args)) method, change the respective entry in the POM.xml file
+ * (simply search for 'mainClass').
  */
 public class DataStreamJob {
 	public final static String INPUT_TOPIC = "gate_of_word";
 	public final static String KAFKA_GROUP = "possession_of_pipeline";
 	public final static String BOOTSTAP_SERVERS = "localhost:9092";
-	public final static String URL = "jdbc:postgresql://localhost:5432/db_counted_word";
+	public final static String URL = "jdbc:postgresql://localhost:5432/counted_words";
 	public final static String SQL_DRIVER = "org.postgresql.Driver";
-	
+
 	public final static String USERNAME = "postgres";
 	public final static String PASSWORD = "1111";
 	public final static String NAME_OF_STREAM = "Kafka Source";
-	public final static String COLOMN_OF_RESULT = "count";
+	public final static String COLOMN_OF_RESULT = "number";
 	public final static String NAME_OF_FLINK_JOB = "Flink Job";
-	public final static String SELECT_SQL_QUERY = "SELECT * FROM mytable WHERE id = ?";
-	public final static String INSERT_SQL_QUERY = "INSERT INTO table (count, word) VALUES (?, ?)";
-	public final static String UPDATE_SQL_QUERY = "UPDATE table SET count = ? WHERE word = ?";
-	
-	
+	public final static String SELECT_SQL_QUERY = "SELECT * FROM counted_words WHERE word = ?";
+	public final static String INSERT_SQL_QUERY = "INSERT INTO counted_words (word, number) VALUES (?, ?)";
+	public final static String UPDATE_SQL_QUERY = "UPDATE counted_words SET number = ? WHERE word = ?";
+
 	public static void main(String[] args) throws Exception {
 		// Sets up the execution environment, which is the main entry point
 		// to building Flink applications.
@@ -73,9 +77,9 @@ public class DataStreamJob {
 
 		KafkaSource<String> source = KafkaSource.<String>builder().setBootstrapServers(BOOTSTAP_SERVERS)
 				.setTopics(INPUT_TOPIC)
-				.setStartingOffsets(OffsetsInitializer.earliest())
-				.setValueOnlyDeserializer(new SimpleStringSchema()).build();
-		
+				.setDeserializer(KafkaRecordDeserializationSchema.valueOnly(StringDeserializer.class))
+				.setUnbounded(OffsetsInitializer.latest()).build();
+
 		DataStream<String> kafkaStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), NAME_OF_STREAM);
 
 		DataStream<Tuple2<String, Integer>> dataFirstMidStream = kafkaStream
@@ -83,13 +87,9 @@ public class DataStreamJob {
 
 					@Override
 					public Tuple2<String, Integer> map(String word) throws Exception {
-						String url = URL;
-						String username = USERNAME;
-						String password = PASSWORD;
 						int count = 0;
-						try (Connection connect = DriverManager.getConnection(url, username, password);
-								PreparedStatement ps = connect
-										.prepareStatement(SELECT_SQL_QUERY);) {
+						try (Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+								PreparedStatement ps = connect.prepareStatement(SELECT_SQL_QUERY);) {
 							ps.setString(1, word);
 							ResultSet resultSet = ps.executeQuery();
 
@@ -97,88 +97,45 @@ public class DataStreamJob {
 								count = resultSet.getInt(COLOMN_OF_RESULT);
 							}
 						}
-						return new Tuple2<>(word, count+1);
+						return new Tuple2<>(word, count + 1);
 					}
 				});
 
-		dataFirstMidStream.addSink(
-                JdbcSink.sink(
-                		INSERT_SQL_QUERY,
-                        (statement, value) -> {
-                        	int count = value.f1;
-            				String word = value.f0;
-                            statement.setString(1, word);
-                            statement.setInt(2, count);
-                        },
-                        JdbcExecutionOptions.builder()
-                                .withBatchSize(1000)
-                                .withBatchIntervalMs(200)
-                                .withMaxRetries(5)
-                                .build(),
-                        new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
-                                .withUrl(URL)
-                                .withDriverName(SQL_DRIVER)
-                                .withUsername(USERNAME)
-                                .withPassword(PASSWORD)
-                                .build()
-                ));
-				
-			
-				
-/*				
-				new RichSinkFunction<Tuple2<String, Integer>>() {
-			private PreparedStatement insertStatement;
-			private PreparedStatement updateStatement;
-			private Connection connection;
+		DataStream<Tuple2<String, Integer>> newWordStrim = dataFirstMidStream
+				.filter(new FilterFunction<Tuple2<String, Integer>>() {
 
-			@Override
-			public void open(Configuration parameters) throws Exception {
-				super.open(parameters);
-				String url = URL;
-				String username = USERNAME;
-				String password = PASSWORD;
-				Connection connection = DriverManager.getConnection(url, username, password);
-				String insertQuery = INSERT_SQL_QUERY;
-				insertStatement = connection.prepareStatement(insertQuery);
-				String updateQuery = UPDATE_SQL_QUERY;
-				updateStatement = connection.prepareStatement(updateQuery);
-			}
+					@Override
+					public boolean filter(Tuple2<String, Integer> value) throws Exception {
+						return value.f1 != 1;
+					}
+				});
 
-			@Override
-			public void invoke(Tuple2<String, Integer> value, Context context) throws Exception {
-				int count = value.f1;
-				String word = value.f0;
+		DataStream<Tuple2<String, Integer>> oldWordStrim = dataFirstMidStream
+				.filter(new FilterFunction<Tuple2<String, Integer>>() {
 
-				if (count == 0) {
-					insertStatement.setString(1, word);
-					insertStatement.setInt(2, count);
-					insertStatement.executeUpdate();
-				} else {
-					updateStatement.setString(1, word);
-					updateStatement.setInt(2, count);
-					updateStatement.executeUpdate();
-				}
-			}
+					@Override
+					public boolean filter(Tuple2<String, Integer> value) throws Exception {
+						return value.f1 == 1;
+					}
+				});
 
-			@Override
-			public void close() throws Exception {
-				super.close();
+		newWordStrim.addSink(JdbcSink.sink(INSERT_SQL_QUERY, (statement, value) -> {
+			int count = value.f1;
+			String word = value.f0;
+			statement.setString(1, word);
+			statement.setInt(2, count);
+		}, JdbcExecutionOptions.builder().withBatchSize(1000).withBatchIntervalMs(200).withMaxRetries(5).build(),
+				new JdbcConnectionOptions.JdbcConnectionOptionsBuilder().withUrl(URL).withDriverName(SQL_DRIVER)
+						.withUsername(USERNAME).withPassword(PASSWORD).build()));
 
-				if (insertStatement != null) {
-					insertStatement.close();
-				}
-
-				if (updateStatement != null) {
-					updateStatement.close();
-				}
-
-				if (connection != null) {
-					connection.close();
-				}
-			}
-		});
-*/
-
+		oldWordStrim.addSink(JdbcSink.sink(UPDATE_SQL_QUERY, (statement, value) -> {
+			int count = value.f1;
+			String word = value.f0;
+			statement.setString(1, word);
+			statement.setInt(2, count);
+		}, JdbcExecutionOptions.builder().withBatchSize(1000).withBatchIntervalMs(200).withMaxRetries(5).build(),
+				new JdbcConnectionOptions.JdbcConnectionOptionsBuilder().withUrl(URL).withDriverName(SQL_DRIVER)
+						.withUsername(USERNAME).withPassword(PASSWORD).build()));
 
 		env.execute("Flink Java API Skeleton");
 	}
