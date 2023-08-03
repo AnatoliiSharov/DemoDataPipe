6fde852f1c11df628695f0b04f575f54ed35d439
anatoliisharov <sharov.anatoliy@gmail.com>
2023-08-03 22:36:49 +0300

    added part of kafka to docker compose. doker-compose don't build

--- kafka_kraft/scripts/create_cluster_id.sh
+++ kafka_kraft/scripts/create_cluster_id.sh
@@ -1,9 +1,1 @@
-#https://medium.com/@katyagorshkova/docker-compose-for-running-kafka-in-kraft-mode-20c535c48b1a
-/bin/bash
-
-file_path="/tmp/clusterID/clusterID"
-
-if [ ! -f "$file_path" ]; then
-  /bin/kafka-storage random-uuid > /tmp/clusterID/clusterID
-  echo "Cluster id has been  created..."
-fi
+kafka-storage random-uuid > /workspace/kafka-ui/documentation/compose/clusterID
