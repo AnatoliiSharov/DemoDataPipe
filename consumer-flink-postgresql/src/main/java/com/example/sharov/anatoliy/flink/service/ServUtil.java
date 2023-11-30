2b583da9e417b2ccd1aac7f24007148a496270c0
anatoliisharov <sharov.anatoliy@gmail.com>
2023-11-30 16:43:30 +0200

    serv and main complite

--- consumer-flink-postgresql/src/main/java/com/example/sharov/anatoliy/flink/service/ServUtil.java
+++ consumer-flink-postgresql/src/main/java/com/example/sharov/anatoliy/flink/service/ServUtil.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.example.sharov.anatoliy.flink.entity.SimilarStoryPojo;
 import com.example.sharov.anatoliy.flink.entity.StoryPojo;
 import com.example.sharov.anatoliy.flink.entity.TagPojo;
@@ -17,8 +20,8 @@ import com.example.sharov.anatoliy.flink.repository.impl.StoryAndTagDaoImpl;
 import com.example.sharov.anatoliy.flink.repository.impl.TagDaoImpl;
 
 public class ServUtil implements Serializable{
-
 	private static final long serialVersionUID = -4587633043735696752L;
+	private static final Logger LOG = LoggerFactory.getLogger(ServUtil.class);
 	
 	private SimilarStoryDao similarStoryDao;
 	private TagDao tagDao;
@@ -34,6 +37,7 @@ public class ServUtil implements Serializable{
 	}
 
 	public void attachSimilarStory(Connection connection, StoryPojo value) throws SQLException {
+		LOG.debug("ServUtil.attachSimilarStory have started with value = {}", value);
 		for (SimilarStoryPojo each : value.getSimilarStories()) {
 
 			if (!similarStoryDao.check(connection, each.getSimilarStory())) {
@@ -46,7 +50,8 @@ public class ServUtil implements Serializable{
 	}
 
 	public void attachTags(Connection connection, StoryPojo value) throws SQLException {
-
+		LOG.debug("ServUtil.attachTags have started with value = {}", value);
+		
 		for (TagPojo each : value.getTags()) {
 
 			if (!tagDao.check(connection, each.getTag())) {
